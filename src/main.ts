6de92c4395dc0bf4eac44f0d9ba97de01bebca53
envis10n <envis10n@envis10n.dev>
2021-05-13 14:02:06 -0500

    Fix page title.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+document.title = "Project Xeon Integrated Terminal";
+
 const app = createApp(App);
 
 app.provide("websocket_uri", process.env.VUE_APP_WEBSOCKET_URI);
